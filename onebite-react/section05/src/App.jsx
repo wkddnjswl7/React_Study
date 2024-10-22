0442fbe7584046b0eaa7fa9ffec252961dd2832b
wkddnjswl7 <wkddnjswl7@naver.com>
2024-10-22 20:34:04 +0900

    10/22 React 공부

---
+++ onebite-react/section05/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/main";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+      <h1>안녕 리액트!</h1>
+    </>
+  );
+}
+
+export default App;
