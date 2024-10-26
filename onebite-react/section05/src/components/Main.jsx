8f9317f76cb6d54eaeb940bc4d215a8b6c91e072
wkddnjswl7 <wkddnjswl7@naver.com>
2024-10-27 03:11:31 +0900

    2024/10/27 5-4 강 수업

--- onebite-react/section05/src/components/Main.jsx
+++ onebite-react/section05/src/components/Main.jsx
@@ -1,9 +1,19 @@
+import "./Main.css";
+
 const Main = () => {
-  return (
-    <main>
-      <h1>main</h1>
-    </main>
-  );
+  const user = {
+    // 현재 사이트에 접속한 유저의 상태르 저장
+    name: "이정환",
+    isLogin: true,
+  };
+
+  // return <>{user.isLogin ? <div>로그아웃</div> : <div>로그인</div>}</>;
+
+  if (user.isLogin) {
+    return <div className="logout">로그아웃</div>;
+  } else {
+    return <div>로그인</div>;
+  }
 };
 
 export default Main;
