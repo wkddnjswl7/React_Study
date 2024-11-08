904535117628a2910294ad098c25d89fad1f4ff7
wkddnjswl7 <wkddnjswl7@naver.com>
2024-11-08 22:46:32 +0900

    2024/11/05 react(5.5) 이벤트 핸들링

--- onebite-react/section05/src/components/button.jsx
+++ onebite-react/section05/src/components/button.jsx
@@ -1,7 +1,18 @@
 const Button = ({ text = "기본 텍스트", color = "black", children }) => {
   console.log(text, color);
+
+  //이벤트 객체 : 이벤트 핸들러 함수를 호출하면서 호출된 이벤트 핸들러 함수에 매개변수로 이벤트 객체라는 것을 제공
+  const onClickButton = (e) => {
+    console.log(e);
+    console.log(text);
+  };
+
   return (
-    <button style={{ color: color }}>
+    <button
+      onClick={onClickButton}
+      //onMouse={onClickButton}
+      style={{ color: color }}
+    >
       {text} - {color.toUpperCase()}
       {children}
     </button>
