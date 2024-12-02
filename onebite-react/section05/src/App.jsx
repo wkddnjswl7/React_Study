4c01a7799fc01b3c6f5b58927b802408ff21fe63
Unji <121096466+wkddnjswl7@users.noreply.github.com>
2024-12-02 23:46:08 +0900

    [2024/12/2] State와 Props 공부  (#6)
    * 10/22 React 공부
    
    * 2024/10/27 5-4 강 수업
    
    * 2024/11/05 react(5.5) 이벤트 핸들링
    
    * 11/8 이벤트 핸들러 병합 충돌 해결
    
    * 2024/11/13 5.6 State로 상태관리하기
    
    * 2024/12/02 state와 props 공부

--- onebite-react/section05/src/App.jsx
+++ onebite-react/section05/src/App.jsx
@@ -1,52 +1,25 @@
 import "./App.css";
-import { useState } from "react";
+import Bulb from "./components/Bulb";
+import Counter from "./components/Counter";
+
+//컴포넌트는 리렌더링이 발생하는 3가지
+// 1. 자신이 관리하는state의 값이 변경될 때
+// 2. 제공받는 props의 값이 변경될 때
+// 3. 부모 컴포넌트가 리렌더링되면 자식 컴포넌트도 리렌더링
+
+// 부모 컴퍼넌트로부터 props로 전구의 상태를 light로 받아온다.
+//{light} = {props} 구조분해할당
+//구조 분해 할당(Destructuring Assignment)은 객체나 배열에 있는 값을 쉽게 꺼내서 변수에 저장할 수 있도록 해주는 JavaScript 문법
+// Bulb와 같은 자식 컴포넌트는 부모로부터 받는 Props의 값이 바뀌면 리렌덩이 발생
+
 
 function App() {
-  //const state = useState(0);
-  //1. const [state, setState] = useState(0); // 구조 분해로 하여 state : 현재값, setState : 상태 변화 함수
-  //console.log(state);
-  // state 값은 { 0 : undefined, 1 : 함수 } 이렇게 배열로 되어 있다.
-  // 첫 번재 요소는 새롭게 생성된 state 값 -> 현재 값
-  // 두 번째 요소는 state 값을 변경, 변화 시키는 함수(상태 변화 함수)
-  // 컴퍼넌트의 스테이트 값이 바뀌면 이 컴퍼넌트가 리턴을 한다 -> 리렌더링
-
-  // 2.
-  const [count, setCount] = useState(0);
-
-  // 3.
-  const [light, setLight] = useState("off");
-
-  // state를 사용하지않고 일반 변수로 작성할 경우 컴퍼넌트가 리렌더링 되지않는다.
+  
   return (
     <>
-      <div>
-        <h1>{light}</h1>
-        <button
-          onClick={() => {
-            // if (light == "off") {
-            //   setLight("on");
-            // } else {
-            //   setLight("off");
-            // }
+      <Bulb />
+      <Counter />
 
-            //삼항 연산자 사용
-            setLight(light === "on" ? "off" : "on");
-          }}
-        >
-          Click
-        </button>
-      </div>
-
-      <div>
-        <button
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          Click
-        </button>
-        <h1>{count}</h1>
-      </div>
     </>
   );
 }
---
+++ onebite-react/section05/src/components/Counter.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+  
+    return(
+      <div>
+        <h1>{count}</h1>
+        <button onClick={() => {
+          setCount(count + 1);
+        }}>
+          Click
+        </button>
+      </div>
+    )
+  };
+
+export default Counter;
